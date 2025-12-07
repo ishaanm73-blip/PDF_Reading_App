3136413be064aa4fa24ea72bec677fd59faed7f7
ishaanm73-blip <ishaanm73@gmail.com>
2025-12-07 10:04:46 +0000

    commit Update llm.py and related files

--- backend/app/ser/llm.py
+++ backend/app/ser/llm.py
@@ -4,7 +4,6 @@ import os
 genai.configure(api_key=os.getenv("AIzaSyAR8RNXS6MBbGhEAOrGmptcKkCXoQF7mmw"))
 model = genai.GenerativeModel("gemini-2.0-flash-lite")
 
-def answer(question, context):
-    prompt = f"Context:\n{context}\n\nQuestion: {question}\n\nAnswer:"
+def answer(prompt:str):
     response = model.generate_content(prompt)
     return response.text
